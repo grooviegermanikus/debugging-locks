88c9eb53cfa4ac682f5ae900fb97a026a16260ff
steve_gg <grooviegermanikus@gmail.com>
2023-03-13 22:03:15 +0100

    got simple example working

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,10 +1,42 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
 use env_logger::Env;
+use rust_debugging_locks::debugging_locks::RwLockWrapped;
 
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
-    // FIXME
-    // debugging_locks_run::runit();
+    runit();
 //
 }
+
+
+fn runit() {
+
+
+    let lock : Arc<RwLockWrapped<HashMap<i32,i32>>> = Arc::new(RwLockWrapped::new(HashMap::new()));
+
+    let l1 = lock.clone();
+    thread::spawn(move || {
+        let r1 = l1.read().unwrap();
+        println!("acquire read lock {} ...", r1.len());
+        thread::sleep(Duration::from_millis(2000));
+        println!("... release read lock");
+    });
+
+    thread::sleep(Duration::from_millis(500));
+
+    {
+
+        // let mut w = lock.write().unwrap();
+        let mut _w = lock.write().unwrap();
+        // exclusive lock -> wait
+
+    }
+
+
+}
+
 
