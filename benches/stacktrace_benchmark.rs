057ae5321f3495dc3da0aace348d5df34673fda0
GroovieGermanikus <groovie@mango.markets>
2024-06-24 17:12:30 +0200

    fix compile

--- benches/stacktrace_benchmark.rs
+++ benches/stacktrace_benchmark.rs
@@ -3,27 +3,25 @@ use rust_debugging_locks::stacktrace_util::backtrack_frame;
 
 fn dummy_start_frame() {
     let stacktrace = backtrack_frame(|symbol_name| symbol_name.contains("no_skip"));
-    assert_eq!(3, stacktrace.unwrap().len());
+    assert_eq!(3, stacktrace.unwrap().frames.len());
 }
 
 fn backtrace_benchmark(c: &mut Criterion) {
-    c.bench_function("backtrace full stack", |b| b.iter(||
-        {
+    c.bench_function("backtrace full stack", |b| {
+        b.iter(|| {
             backtrace::trace(|_frame| {
                 true // continue
             });
-        }
-    ));
-    c.bench_function("backtrace not walking stack", |b| b.iter(||
-        {
+        })
+    });
+    c.bench_function("backtrace not walking stack", |b| {
+        b.iter(|| {
             backtrace::trace(|_frame| {
                 false // do not continue
             });
-        }
-    ));
-    c.bench_function("stacktrace", |b| b.iter(||
-        dummy_start_frame()
-    ));
+        })
+    });
+    c.bench_function("stacktrace", |b| b.iter(|| dummy_start_frame()));
 }
 
 criterion_group!(benches, backtrace_benchmark);
